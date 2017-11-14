f066023b9a82ad90176ecf14a274db582b162cc7
Satnam Nandra <SatnamNandra@yahoo.com>
2017-11-14 21:48:10 +0000

    mvc middleware added

--- EuroTrim.api/Startup.cs
+++ EuroTrim.api/Startup.cs
@@ -15,6 +15,7 @@ namespace EuroTrim.api
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -23,6 +24,10 @@ namespace EuroTrim.api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler();
             }
 
             app.Run(async (context) =>
