994b7799096e45bfd1cde242b8937fbf0e3a9406
Satnam Nandra <SatnamNandra@yahoo.com>
2017-11-16 23:06:29 +0000

    if serielization properties are required for older formats - json camel casing is the default

--- EuroTrim.api/Startup.cs
+++ EuroTrim.api/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
 
 namespace EuroTrim.api
 {
@@ -16,6 +17,17 @@ namespace EuroTrim.api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+                //.AddJsonOptions(o =>
+                //{
+                //    if (o.SerializerSettings.ContractResolver != null)
+                //    {
+                //        var castedResolver = o.SerializerSettings.ContractResolver
+                //            as DefaultContractResolver;
+                //            castedResolver.NamingStrategy = null;
+                      
+
+                //    }
+                //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
