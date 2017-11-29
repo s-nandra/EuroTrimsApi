4eb75818317844c576687f70989c3a7e484b3eb3
Satnam Nandra <SatnamNandra@yahoo.com>
2017-11-29 22:49:00 +0000

    Update and Patch added

--- EuroTrim.api/Controllers/ProductController.cs
+++ EuroTrim.api/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using EuroTrim.api.Models;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -70,7 +71,7 @@ namespace EuroTrim.api.Controllers
             {
                 return BadRequest(ModelState);
             }
-            
+
             var products = ProductsDataStore.Current.Products.ToList();
             var maxprodid = products.Max(p => p.Id);
 
@@ -91,7 +92,119 @@ namespace EuroTrim.api.Controllers
 
             return CreatedAtRoute("GetProduct", new
             { id = newProduct.Id }, newProduct);
-             
+
+        }
+
+        [HttpPut("api/products/{id}")]
+        public IActionResult UpdateProduct(int id,
+            [FromBody] ProductForUpdateDto product)
+        {
+            if (product == null)
+            {
+                return BadRequest();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var productToUpdate = ProductsDataStore.Current.Products.FirstOrDefault(
+                p => p.Id == id);
+
+            if (productToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            productToUpdate.PartNo = product.PartNo;
+            productToUpdate.ProdName = product.ProdName;
+
+            //product.Category - product.Category;
+            productToUpdate.Description = product.Description;
+            productToUpdate.Colour = product.Colour;
+            productToUpdate.Size = product.Size;
+            productToUpdate.BuyPrice = product.BuyPrice;
+            productToUpdate.Discount1 = product.Discount1;
+            productToUpdate.Discount2 = product.Discount2;
+            productToUpdate.Discount3 = product.Discount3;
+            productToUpdate.Discount4 = product.Discount4;
+
+
+            return NoContent();
+
+        }
+
+        [HttpPatch("api/products/{id}")]
+        public IActionResult PartiallyUpdateProduct(int id,
+            [FromBody] JsonPatchDocument<ProductForUpdateDto> product)
+        {
+            if (product == null)
+            {
+                return BadRequest();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var productToUpdate = ProductsDataStore.Current.Products.FirstOrDefault(
+                p => p.Id == id);
+
+            if (productToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            var patchProduct =
+                new ProductForUpdateDto()
+                {
+
+                    PartNo = productToUpdate.PartNo,
+                    ProdName = productToUpdate.ProdName,
+                    //product.Category - product.Category;
+                    Description = productToUpdate.Description,
+                    Colour = productToUpdate.Colour,
+                    Size = productToUpdate.Size,
+                    BuyPrice = productToUpdate.BuyPrice,
+                    Discount1 = productToUpdate.Discount1,
+                    Discount2 = productToUpdate.Discount2,
+                    Discount3 = productToUpdate.Discount3,
+                    Discount4 = productToUpdate.Discount4
+                };
+
+
+            product.ApplyTo(patchProduct, ModelState);
+
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+
+            }
+
+            if(productToUpdate.ProdName == patchProduct.Description)
+            {
+                ModelState.AddModelError("Description", "Description cant be same as name");
+            }
+
+            TryValidateModel(patchProduct);
+
+            productToUpdate.PartNo = patchProduct.PartNo;
+            productToUpdate.ProdName = patchProduct.ProdName;
+
+            //product.Category - product.Category;
+            productToUpdate.Description = patchProduct.Description;
+            productToUpdate.Colour = patchProduct.Colour;
+            productToUpdate.Size = patchProduct.Size;
+            productToUpdate.BuyPrice = patchProduct.BuyPrice;
+            productToUpdate.Discount1 = patchProduct.Discount1;
+            productToUpdate.Discount2 = patchProduct.Discount2;
+            productToUpdate.Discount3 = patchProduct.Discount3;
+            productToUpdate.Discount4 = patchProduct.Discount4;
+
+            return NoContent();
+
         }
     }
 }
---
+++ EuroTrim.api/Models/ProductForUpdateDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EuroTrim.api.Models
+{
+    public class ProductForUpdateDto
+    {
+        [Required(ErrorMessage = "Please enter a part number")]
+        [MaxLength(100)]
+        public string PartNo { get; set; }
+
+        public string ProdName { get; set; }
+
+        public string Description { get; set; }
+
+        public CategoryDto Category { get; set; }
+
+        public int Per { get; set; }
+        public string Colour { get; set; }
+        public string Size { get; set; }
+        public decimal BuyPrice { get; set; }
+        public decimal DutyPrice { get; set; }
+        public decimal ListPrice { get; set; }
+        public decimal Discount1 { get; set; }
+        public decimal Discount2 { get; set; }
+        public decimal Discount3 { get; set; }
+        public decimal Discount4 { get; set; }
+
+    }
+}
