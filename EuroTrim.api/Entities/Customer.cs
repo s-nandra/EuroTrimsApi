86c38885faa0ef8f74d30b5856d1bfa1416e0c00
Satnam Nandra <SatnamNandra@yahoo.com>
2018-02-01 21:18:17 +0000

    migrations updated

--- EuroTrim.api/Entities/Customer.cs
+++ EuroTrim.api/Entities/Customer.cs
@@ -10,11 +10,31 @@ namespace EuroTrim.api.Entities
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
- 
+
+        [Required]
+        [MaxLength(50)]
         public string Name { get; set; }
-        public string Decription { get; set; }
+
+        [MaxLength(80)]
+        public string Company { get; set; }
+
+        [MaxLength(50)]
         public int ContactNumber { get; set; }
- 
+
+        [MaxLength(50)]
+        public string Address1 { get; set; }
+
+        [MaxLength(50)]
+        public string Address2 { get; set; }
+
+        [MaxLength(20)]
+        public string City { get; set; }
+
+        [MaxLength(10)]
+        public string PostCode { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
         public ICollection<Product> Product  { get; set; } = new List<Product>();
     }
 }
--- EuroTrim.api/Migrations/20180201211556_CustomerFieldsAdded.Designer.cs
+++ EuroTrim.api/Migrations/20180201211556_CustomerFieldsAdded.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace EuroTrim.api.Migrations
 {
     [DbContext(typeof(EuroTrimContext))]
-    partial class EuroTrimContextModelSnapshot : ModelSnapshot
+    [Migration("20180201211556_CustomerFieldsAdded")]
+    partial class CustomerFieldsAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
