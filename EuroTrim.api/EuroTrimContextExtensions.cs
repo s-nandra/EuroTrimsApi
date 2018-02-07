ea38a81272022aa5d95cc5574916b5e14167babf
Satnam Nandra <SatnamNandra@yahoo.com>
2018-02-07 10:43:13 +0000

    Seeded db

---
+++ EuroTrim.api/EuroTrimContextExtensions.cs
@@ -0,0 +1,91 @@
+﻿using EuroTrim.api.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EuroTrim.api
+{
+    public static class EuroTrimExtensions
+    {
+        public static void EnsureSeedDataForContext(this EuroTrimContext context)
+        {
+            if (context.Customers.Any())
+            {
+                return;
+            }
+
+
+            var customers = new List<Customer>()
+            {
+                new Customer()
+                {
+
+                    Name = "Satnam Nandra",
+                    Address1 = "22 tree st",
+                    Address2 = "Earley",
+                    PostCode = "MG1 1HL",
+                    Product = new List<Product>()
+                    {
+                        new Product()
+                        {
+
+                            ProdName="xxx",
+                            Description="my xxx product",
+                            Category = new Category()
+                            {
+                                Name="Zipper",
+                                IsActive=true
+                            }
+                        },
+                        new Product()
+                        {
+
+                            ProdName="yyy",
+                            Description="my 2nd prod",
+                            Category = new Category()
+                            {
+                                Name="Fastenings",
+                                IsActive=true
+                            }
+                        }
+                    }
+                },
+                new Customer()
+                {
+                    Name = "Indy Sogi",
+                    Address1 = "32 xyz",
+                    PostCode = "77D Duu",
+                    Product = new List<Product>()
+                    {
+                        new Product()
+                        {
+                            ProdName="xxx",
+                            Description="my xxx product",
+                            Category = new Category()
+                            {
+                                Name="Zipper",
+                                IsActive=true
+                            }
+                        }
+                    }
+                },
+                new Customer()
+                {
+                    Name = "john Doe",
+                    Address1 = "22 Joe st",
+                    
+                    Address2 = "Nowhwre",
+                    City= "Anywhere",
+                    PostCode="TG4 6HH"
+                    
+                }
+            };
+
+            context.Customers.AddRange(customers);
+            context.SaveChanges();
+
+
+        }
+    }
+}
--- EuroTrim.api/Startup.cs
+++ EuroTrim.api/Startup.cs
@@ -57,7 +57,8 @@ namespace EuroTrim.api
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory,
+            EuroTrimContext euroTrimContext)
         {
             loggerFactory.AddConsole();
 
@@ -71,6 +72,8 @@ namespace EuroTrim.api
             {
                 app.UseExceptionHandler();
             }
+
+            euroTrimContext.EnsureSeedDataForContext();
 
             app.UseStatusCodePages();
 
